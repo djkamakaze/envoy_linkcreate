e691c5c2c8ebca7fe94139536bd8c9070f4940d6
Keith Kehrer <djkamakaze@gmail.com>
2018-05-16 07:40:08 +0000

    Updates to LinkCreate

--- Block/LinkCreate.php
+++ Block/LinkCreate.php
@@ -1,13 +1,34 @@
 <?php
 namespace Envoy\LinkCreate\Block;
- 
 class LinkCreate extends \Magento\Framework\View\Element\Template
-{
-    public function getLinkCreate()
+{	
+	protected $_productRepository;
+		
+	public function __construct(
+		\Magento\Backend\Block\Template\Context $context,		
+		\Magento\Catalog\Model\ProductRepository $productRepository,
+		array $data = []
+	)
+	{
+		$this->_productRepository = $productRepository;
+		return parent::__construct($context, $data);
+	}
+	
+	public function getProductById($id)
+	{
+		return $this->_productRepository->getById($id);
+	}
+	
+	public function getProductBySku($sku)
+	{
+		return $this->_productRepository->get($sku);
+	}
+	public function getLinkCreate()
     {
     	$prodId = $this->getRequest()->getParam('id');
-		// echo $prodId;
+    	
 		return $prodId;
-        // return 'Create a Linkdfhdfhdfgh';
+       
     }
 }
+?>
